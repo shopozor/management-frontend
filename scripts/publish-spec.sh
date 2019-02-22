4c3443267497b494968abaa2d86934894f632c9e
Laurent Michel <laurent.michel@softozor.ch>
2019-02-22 12:18:36 +0100

    restore spec publication script

--- scripts/publish-spec.sh
+++ scripts/publish-spec.sh
@@ -1,1 +1,3 @@
-scp -r specification/* michella@softozor.ch:~/www/www.softozor.ch/shopozor/management-frontend/
+TARGET_PATH="~/www/www.softozor.ch/shopozor/management-frontend/"
+ssh michella@softozor.ch "cd $TARGET_PATH; rm -Rf *"
+scp -r specification/* michella@softozor.ch:$TARGET_PATH

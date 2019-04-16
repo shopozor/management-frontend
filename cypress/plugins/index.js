b32beceadeab69a9eb0a93facc69559abaa63794
Laurent Michel <laurent.michel@softozor.ch>
2019-04-16 13:11:53 +0200

    factor out common cypress plugin code

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,21 +11,10 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const fs = require('fs-extra')
 const cucumber = require('cypress-cucumber-preprocessor').default
-const path = require('path')
 // const webpack = require('@cypress/webpack-preprocessor')
 
-function getConfigurationByFile (file) {
-  const pathToConfigFile = path.resolve('common', 'cypress', 'config', `${file}.json`)
-  console.log('loading cypress config file: ', pathToConfigFile)
-  return fs.readJson(pathToConfigFile)
-}
-
-function getConfiguration (config) {
-  const file = config.env.configFile || 'development'
-  return getConfigurationByFile(file)
-}
+const helpers = require('../../common/cypress/plugins/helpers')
 
 module.exports = (on, config) => {
   // const options = {
@@ -42,5 +31,5 @@ module.exports = (on, config) => {
   on('file:preprocessor', file => { return cucumber()(file) })
   // on('file:preprocessor', file => webpack(options)(file))
 
-  return getConfiguration(config)
+  return helpers.getConfiguration(config)
 }

7de3c9b0eb9e4a971b3d609c80c762736b12e5b3
Laurent Michel <laurent.michel@softozor.ch>
2019-04-29 14:41:39 +0200

    try to factor out deployment to external lib

--- e2e/TestLib.groovy
+++ e2e/TestLib.groovy
@@ -1,10 +1,24 @@
-def myTest() {
+// def myTest(backendJPS, backendNamePsw, ) {
+//   environment {
+//     GITHUB_CREDENTIALS = credentials('github-credentials')
+//     BACKEND_JPS = 'backend.jps'
+//     // CI_HOSTNAME = JENKINS_URL.split('/')[2].split(':')[0]
+//   }
+//   sh "curl -o $BACKEND_JPS $BACKEND_NAME_PSW"
+//   sh "sed -i \"s/GIT_USER/$GITHUB_CREDENTIALS_USR/g\" $BACKEND_JPS"
+//   sh "sed -i \"s/GIT_PASSWORD/$GITHUB_CREDENTIALS_PSW/g\" $BACKEND_JPS"
+//   sh "sed -i \"s/GIT_BRANCH/dev/g\" $BACKEND_JPS"
+//   sh "chmod u+x ./common/e2e/deploy-to-jelastic.sh"
+//   sh "./common/e2e/deploy-to-jelastic.sh $JELASTIC_APP_CREDENTIALS_USR $JELASTIC_APP_CREDENTIALS_PSW $JELASTIC_CREDENTIALS_USR $JELASTIC_CREDENTIALS_PSW $BACKEND_NAME_USR cp $BACKEND_JPS"
+// }
+
+def deploy(backendJps, backendEnvName) {
   environment {
-    GITHUB_CREDENTIALS = credentials('github-credentials')
-    BACKEND_JPS = 'backend.jps'
-    // CI_HOSTNAME = JENKINS_URL.split('/')[2].split(':')[0]
+    JELASTIC_APP_CREDENTIALS = credentials('jelastic-app-credentials')
+    JELASTIC_CREDENTIALS = credentials('jelastic-credentials')
   }
-  echo "hello world: $GITHUB_CREDENTIALS_USR"
+  sh "chmod u+x ./common/e2e/deploy-to-jelastic.sh"
+  sh "./common/e2e/deploy-to-jelastic.sh $JELASTIC_APP_CREDENTIALS_USR $JELASTIC_APP_CREDENTIALS_PSW $JELASTIC_CREDENTIALS_USR $JELASTIC_CREDENTIALS_PSW $backendEnvName cp $backendJps"
 }
 
 return this;

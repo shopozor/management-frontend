a3eba3dfdd8960380e9c070fa1d4ada7ceb7a860
Laurent Michel <laurent.michel@softozor.ch>
2019-04-29 14:30:08 +0200

    try to use env vars

--- e2e/TestLib.groovy
+++ e2e/TestLib.groovy
@@ -1,5 +1,10 @@
 def myTest() {
-  echo "hello world"
+  environment {
+    GITHUB_CREDENTIALS = credentials('github-credentials')
+    BACKEND_JPS = 'backend.jps'
+    // CI_HOSTNAME = JENKINS_URL.split('/')[2].split(':')[0]
+  }
+  echo "hello world: $GITHUB_CREDENTIALS_USR"
 }
 
 return this;

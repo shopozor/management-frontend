b2b090cf9c94be86295a08d26d9153886c3490fc
Laurent Michel <laurent.michel@softozor.ch>
2019-04-29 17:12:55 +0200

    export backend configuration preparation into a separate lib

--- e2e/TestLib.groovy
+++ e2e/TestLib.groovy
@@ -1,16 +1,10 @@
-// def myTest(backendJPS, backendNamePsw, ) {
-//   environment {
-//     GITHUB_CREDENTIALS = credentials('github-credentials')
-//     BACKEND_JPS = 'backend.jps'
-//     // CI_HOSTNAME = JENKINS_URL.split('/')[2].split(':')[0]
-//   }
-//   sh "curl -o $BACKEND_JPS $BACKEND_NAME_PSW"
-//   sh "sed -i \"s/GIT_USER/$GITHUB_CREDENTIALS_USR/g\" $BACKEND_JPS"
-//   sh "sed -i \"s/GIT_PASSWORD/$GITHUB_CREDENTIALS_PSW/g\" $BACKEND_JPS"
-//   sh "sed -i \"s/GIT_BRANCH/dev/g\" $BACKEND_JPS"
-//   sh "chmod u+x ./common/e2e/deploy-to-jelastic.sh"
-//   sh "./common/e2e/deploy-to-jelastic.sh $JELASTIC_APP_CREDENTIALS_USR $JELASTIC_APP_CREDENTIALS_PSW $JELASTIC_CREDENTIALS_USR $JELASTIC_CREDENTIALS_PSW $BACKEND_NAME_USR cp $BACKEND_JPS"
-// }
+def prepareBackendConfiguration(gitUser, gitPwd, gitBranch, backendJps, backendJpsUrl) {
+  sh "curl -o $backendJps $backendJpsUrl"
+  sh "sed -i \"s/GIT_USER/$gitUser/g\" $backendJps"
+  sh "sed -i \"s/GIT_PASSWORD/$gitPwd/g\" $backendJps"
+  sh "sed -i \"s/GIT_BRANCH/$gitBranch/g\" $backendJps"
+}
+
 
 def deploy(backendJps, backendEnvName) {
   environment {

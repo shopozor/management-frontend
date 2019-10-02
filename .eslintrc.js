7cfbbc33564ae2c56d66966bbf6c98c09d0b253e
Laurent Michel <laurent.michel@ruag.com>
2019-10-02 17:53:53 +0200

    activate husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     sourceType: 'module'
   },
   env: {
-    browser: true
+    browser: true,
+    jest: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
